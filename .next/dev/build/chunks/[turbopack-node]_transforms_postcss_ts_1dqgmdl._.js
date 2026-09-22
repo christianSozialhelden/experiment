35dba16a8592c4fe4044632677149cf46aa9500d
Christian Schade <christian@sozialhelden.de>
2026-09-22 10:24:33 +0200

    init

---
+++ .next/dev/build/chunks/[turbopack-node]_transforms_postcss_ts_1dqgmdl._.js
@@ -0,0 +1,13 @@
+module.exports = [
+"[turbopack-node]/transforms/postcss.ts?config=[project]/experiment/postcss.config.mjs { CONFIG => \"[project]/experiment/postcss.config.mjs [postcss] (ecmascript)\" } [postcss] (ecmascript, async loader)", ((__turbopack_context__) => {
+
+__turbopack_context__.v((parentImport) => {
+    return Promise.all([
+  "chunks/00o5_0irik_7._.js",
+  "chunks/[root-of-the-server]__0kls_rr._.js"
+].map((chunk) => __turbopack_context__.l(chunk))).then(() => {
+        return parentImport("[turbopack-node]/transforms/postcss.ts?config=[project]/experiment/postcss.config.mjs { CONFIG => \"[project]/experiment/postcss.config.mjs [postcss] (ecmascript)\" } [postcss] (ecmascript)");
+    });
+});
+}),
+];
---
+++ .next/dev/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"qzbg6TUlm+jO8IBAZikm/W0sH90jR9p9zHaqv2zVtGo=\"\n}"
